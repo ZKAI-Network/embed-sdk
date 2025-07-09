1ef13a0ac7ffbe5d1d4547e2b909693455967231
Samuel Huber <samuel@dtech.vision>
2025-07-09 09:50:23 +0200

    add use of feed_id to the sample

--- packages/embed-typescript/examples/embed-typescript-sdk.ts
+++ packages/embed-typescript/examples/embed-typescript-sdk.ts
@@ -2,7 +2,9 @@ import { getClient } from "../src/index.js"
 
 const main = async () => {
   const client = getClient(process.env.API_KEY_EMBED!)
-  const feed = await client.getFeedByUserId("16085", { top_k: 10 })
+  // global trending: Feed ID: feed_388
+  // Your "For you" Feed ID: feed_390
+  const feed = await client.getFeedByUserId("16085", { top_k: 10, feed_id: "feed_388" })
   console.dir(feed, { depth: null })
 }
 
